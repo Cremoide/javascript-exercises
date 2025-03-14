605bb64da3b065b2e99d897989ea32254574a30a
Cremoide <62185350+Cremoide@users.noreply.github.com>
2025-03-14 11:36:02 +0000

    Introduction understood and completed. (01_helloWorld). Second exercise (02_repeatString) completed. (Had issues applying the parameter for the function since I didn't know the test was passing the values for it).

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, count) {
 
+    let repeat = '';
+
+    if (count < 0) {
+        return 'ERROR';
+    }
+
+    for (let i = 0; i < count; i++) {
+        repeat += string;
+    }
+    return repeat;
 };
 
 // Do not edit below this line
