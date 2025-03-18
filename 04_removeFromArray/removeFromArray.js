7072f19359a042ed68eb98e5b813bb20f5c364c9
Cremoide <62185350+Cremoide@users.noreply.github.com>
2025-03-18 12:33:00 +0000

    Fourth exercise (04_removeFromArray) completed. (All tests passed using a for loop, that uses includes() and push() methods).

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...args) {
+    
+    var result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if ((!args.includes(array[i]))) {
+            result.push(array[i]);
+        }
+    }
+
+    console.log(result)
+
+    return result;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
