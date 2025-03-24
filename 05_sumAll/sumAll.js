0814526495957261a5802f214fd74d0b18c16240
Cremoide <62185350+Cremoide@users.noreply.github.com>
2025-03-24 14:04:16 +0000

    Struggled a lot with the Integer check for the variables. 2 more test only on exercise 05 to go!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,44 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
-};
+    // Declares sum value
+    let sum = 0;
+
+    // Check if numbers are positive
+    if (a <= 0 || b <= 0) {
+        return "ERROR"
+    } 
+    
+    if (Number.isInteger(a) === false || (Number.isInteger(b) === false)) {
+        return "ERROR"
+    } 
+
+    console.log()
+
+    // Declares the first, second number and result variables. 
+    const lowVal = Math.min(a, b);
+    const highVal = Math.max(a, b);
+
+    // Reference
+    console.log(highVal);
+    console.log(lowVal);
+
+    // Create a for loop that iterates till reach "highVal" number
+    function numbersBetween(start, end) {
+        return Array.from({length: end - start + 1}, 
+            (_, index) => start + index);
+    }
+    const result = numbersBetween(lowVal, highVal);
+
+    // Reference
+    console.log(result);
+
+    // Create a for loop that iterates till reach "highVal" number
+    for (let i = 0; i < result.length; i++) {
+        sum += result[i];
+    }
+
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
