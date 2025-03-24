be1593bdddabd0b72299905c65f3b6ed66304765
Cremoide <62185350+Cremoide@users.noreply.github.com>
2025-03-24 20:33:37 +0000

    Seventh exercise (07_tempConversion) completed. (All tests passed using the formulas).

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah, cel) {
+
+  cel = (fah - 32) / (9/5)
+
+  if (Number.isInteger(cel) === false) {
+    var roundCel = Math.round(cel * 10) / 10
+    return roundCel;
+  } else {
+    return cel;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel, fah) {
+
+  fah = cel * 9/5 + 32
+
+  if (Number.isInteger(fah) === false) {
+    var roundFah = Math.round(fah * 10) / 10
+    return roundFah;
+  } else {
+    return fah;
+  }
 };
 
 // Do not edit below this line
