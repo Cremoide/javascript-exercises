b20b15b1a2f79a937cb51a85e0907c13231beda2
Cremoide <62185350+Cremoide@users.noreply.github.com>
2025-03-24 16:53:39 +0000

    Fifth exercise (05_SumAll) completed. (All tests passed using a conditional in case of not positive, non-integer or NaN).

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -6,11 +6,17 @@ const sumAll = function(a, b) {
     // Check if numbers are positive
     if (a <= 0 || b <= 0) {
         return "ERROR"
-    } 
-    
+    }
+
+    // Check if numbers are integer
     if (Number.isInteger(a) === false || (Number.isInteger(b) === false)) {
         return "ERROR"
-    } 
+    }
+
+    // Check if numbers are integer
+    if (isNaN(a) === true || (isNaN(b) === true)) {
+        return "ERROR"
+    }
 
     console.log()
 
